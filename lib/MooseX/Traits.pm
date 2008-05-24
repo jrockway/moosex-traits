7a4c84ff341110324357783c5b8922c0c6a75b49
Jonathan Rockway <jon@jrock.us>
2008-05-24 18:54:11 -0500

    add docs

--- lib/MooseX/Traits.pm
+++ lib/MooseX/Traits.pm
@@ -55,3 +55,94 @@ __END__
 =head1 NAME
 
 MooseX::Traits - automatically apply roles at object creation time
+
+=head1 SYNOPSIS
+
+Given some roles:
+
+  package Role;
+  use Moose::Role;
+  has foo => ( is => 'ro', isa => 'Int' required => 1 );
+
+And a class:
+
+  package Class;
+  use Moose;
+  with 'MooseX::Traits';
+
+Apply the roles to the class at C<new> time:
+
+  my $class = Class->new( traits => ['Role'], foo => 42 );
+
+Then use your customized class:
+
+  $class->isa('Class'); # true
+  $class->does('Role'); # true
+  $class->foo; # 42
+
+=head1 DESCRIPTION
+
+Often you want to create components that can be added to a class
+arbitrarily.  This module makes it easy for the end user to use these
+components.  Instead of requiring the user to create a named class
+with the desired roles applied, or applying roles to the instance
+one-by-one, he can just pass a C<traits> parameter to the class's
+constructor.  This role will then apply the roles in one go, and cache
+the resulting class (for efficiency).  Arguments meant to initialize
+the applied roles' attributes can also be passed to the constructor.
+(This will probably break L<MooseX::StrictConstructor>.)
+
+=head1 ATTRIBUTES YOUR CLASS GETS
+
+This role will add the following attributes to the consuming class.
+
+=head2 traits
+
+The list of traits to be applied.  Acts as an C<init_arg> and a C<ro>
+accessor.
+
+=head2 _trait_namespace
+
+You can override the value of this attribute with C<default> to
+automatically prepend a namespace to the supplied traits.  (This can
+be overridden by prefixing the trait name with C<+>.)
+
+Example:
+
+  package Another::Trait;
+  use Moose::Role;
+  has 'bar' => (
+      is       => 'ro',
+      isa      => 'Str',
+      required => 1,
+  );
+
+  package Another::Class;
+  use Moose;
+  with 'MooseX::Traits';
+  has '+_trait_namespace' => ( default => 'Another' );
+ 
+  my $instance = Another::Class->new(
+      traits => ['Trait'], # "Another::Trait", not "Trait"
+      bar    => 'bar',
+  );
+
+=head1 BUGS
+
+This role wraps C<new> with C<around>, which probably isn't the best
+idea ever conceived.  I might change this implementation detail at a
+later date.
+
+=head1 AUTHOR
+
+Jonathan Rockway C<< <jrockway@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008 Infinity Interactive, Inc.
+
+L<http://www.iinteractive.com>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
